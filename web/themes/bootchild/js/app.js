43cf6faf0f002fc35e5ddd0fc5529382facbb5eb
Elisa Demanez <demanez.elisa@gmail.com>
2018-04-26 11:02:11 +0200

    hiding 'afficher poids des lignes' button when creating a DSO

--- web/themes/bootchild/js/app.js
+++ web/themes/bootchild/js/app.js
@@ -1,14 +1,23 @@
+if(window.location.href.toString().includes('node/add')) {
+    
+    document.querySelectorAll('.tabledrag-toggle-weight').forEach( x => {x.style.display = NavigationEvent})
+}
+
+// 
+
+
 var isADV = document.querySelector('body').classList.contains('role-ad') 
 
 if (isADV) {
 
-document.querySelector('[href="/liste"]').style.display = "none";
+    document.querySelector('[href="/liste"]').style.display = "none";
 }
 
 if(!isADV) {
 
-document.querySelector('[href="/admin/people"]').style.display = "none";
+    document.querySelector('[href="/admin/people"]').style.display = "none";
 
 }
+
 
 
